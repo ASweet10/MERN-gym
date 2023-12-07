39ca39ab5c1284188890b569d42ce70b3476c190
ASweet10 <asweet1234@yahoo.com>
2023-12-07 03:42:06 -0600

    updated

--- backend/server.js
+++ backend/server.js
@@ -5,15 +5,11 @@ const express = require('express')
 const mongoose = require("mongoose")
 const blogRoutes = require('./routes/blogs')
 const userRoutes = require('./routes/user')
-const cors = require('cors')
-const router = require('./routes/blogs')
 
 const app = express() // Express app
 
 // Express Middleware
 app.use(express.json()) // Checks incoming requests
-app.use(cors())
-app.use(cors({origin: 'https://mern-gym.vercel.app'}))
 
 app.use((req, res, next) => {
     console.log(req.path, req.method)
